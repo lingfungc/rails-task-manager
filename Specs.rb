5c581a3a03d81ee0e23d21bef741c8e7a68d2377
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-14 23:14:33 +0000

    added checkbox for completed status of the tasks

--- Specs.rb
+++ Specs.rb
@@ -4,3 +4,8 @@ rails db:create db:migrate
 
 2 - controller
 rails g controller tasks
+
+3 - list tasks
+i   routes.rb -> get "/tasks", to: "tasks#index"
+ii  tasks_controller.rb -> def index { @tasks = Task.all } end
+iii index.html.erb -> @tasks.each do |task| ... end
